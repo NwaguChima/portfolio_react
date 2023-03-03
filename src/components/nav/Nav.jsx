e4ef70babedb5ca415e2455266c5406a69541bfc
NwaguChima <nwaguchima48@gmail.com>
2023-03-03 17:27:01 +0100

    setup nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
+import { IoHomeOutline } from 'react-icons/io5';
+import { FaUserAlt, FaBook } from 'react-icons/fa';
+import { MdOutlineMiscellaneousServices, MdContactPage } from 'react-icons/md';
+
 import './nav.css';
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <nav>
+      <a href="#">
+        <IoHomeOutline />
+      </a>
+      <a href="#about">
+        <FaUserAlt />
+      </a>
+      <a href="#experience">
+        <FaBook />
+      </a>
+      <a href="#services">
+        <MdOutlineMiscellaneousServices />
+      </a>
+      <a href="#contact">
+        <MdContactPage />
+      </a>
+    </nav>
+  );
 };
 
 export default Nav;
