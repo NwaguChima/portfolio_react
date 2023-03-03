4e63f4d0f30de9e52f71442772ffbd3587c5fc95
NwaguChima <nwaguchima48@gmail.com>
2023-03-03 17:39:50 +0100

    setup active nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IoHomeOutline } from 'react-icons/io5';
 import { FaUserAlt, FaBook } from 'react-icons/fa';
 import { MdOutlineMiscellaneousServices, MdContactPage } from 'react-icons/md';
@@ -6,21 +6,43 @@ import { MdOutlineMiscellaneousServices, MdContactPage } from 'react-icons/md';
 import './nav.css';
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#');
+
   return (
     <nav>
-      <a href="#">
+      <a
+        href="#"
+        onClick={() => setActiveNav('#')}
+        className={activeNav === '#' ? 'active' : ''}
+      >
         <IoHomeOutline />
       </a>
-      <a href="#about">
+      <a
+        href="#about"
+        onClick={() => setActiveNav('#about')}
+        className={activeNav === '#about' ? 'active' : ''}
+      >
         <FaUserAlt />
       </a>
-      <a href="#experience">
+      <a
+        href="#experience"
+        onClick={() => setActiveNav('#experience')}
+        className={activeNav === '#experience' ? 'active' : ''}
+      >
         <FaBook />
       </a>
-      <a href="#services">
+      <a
+        href="#services"
+        onClick={() => setActiveNav('#services')}
+        className={activeNav === '#services' ? 'active' : ''}
+      >
         <MdOutlineMiscellaneousServices />
       </a>
-      <a href="#contact">
+      <a
+        href="#contact"
+        onClick={() => setActiveNav('#contact')}
+        className={activeNav === '#contact' ? 'active' : ''}
+      >
         <MdContactPage />
       </a>
     </nav>
