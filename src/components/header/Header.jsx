28456775b971936532c5579c71d76a043926ee98
NwaguChima <nwaguchima48@gmail.com>
2023-03-03 16:47:06 +0100

    setup cta

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import CTA from './CTA';
 import './header.css';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello, I'm</h5>
+        <h1>Chima Nwagu</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+
+        <CTA />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
