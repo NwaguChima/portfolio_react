d41b71257c0a5c7a2ad4a84f1dce9877b2b846fe
NwaguChima <nwaguchima48@gmail.com>
2023-03-04 08:23:21 +0100

    added first article markup

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,8 +1,46 @@
 import React from 'react';
+import { GiCheckMark } from 'react-icons/gi';
 import './services.css';
 
 const Services = () => {
-  return <section id="services">Services</section>;
+  return (
+    <section id="services">
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className="container services__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>UI/UX Design</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <GiCheckMark className="service__list-icon" />
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <GiCheckMark className="service__list-icon" />
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <GiCheckMark className="service__list-icon" />
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <GiCheckMark className="service__list-icon" />
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <GiCheckMark className="service__list-icon" />
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </li>
+          </ul>
+        </article>
+        {/* end of first article */}
+      </div>
+    </section>
+  );
 };
 
 export default Services;
