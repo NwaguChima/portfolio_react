71b6f7a69c80da6b1fe24e21b7df022a8112142c
NwaguChima <nwaguchima48@gmail.com>
2023-03-04 08:23:55 +0100

    added final article

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -40,7 +40,39 @@ const Services = () => {
         {/* end of first article */}
         <article className="service">
           <div className="service__head">
-            <h3>UI/UX Design</h3>
+            <h3>Web Development</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <GiCheckMark className="service__list-icon" />
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <GiCheckMark className="service__list-icon" />
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <GiCheckMark className="service__list-icon" />
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <GiCheckMark className="service__list-icon" />
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <GiCheckMark className="service__list-icon" />
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <GiCheckMark className="service__list-icon" />
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </li>
+          </ul>
+        </article>
+        <article className="service">
+          <div className="service__head">
+            <h3>Content Creation</h3>
           </div>
 
           <ul className="service__list">
