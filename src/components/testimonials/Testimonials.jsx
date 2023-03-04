04ca0398674e19c2e832cb2757fb206ede0e789a
NwaguChima <nwaguchima48@gmail.com>
2023-03-04 09:30:51 +0100

    completed markup

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -25,6 +25,45 @@ const Testimonials = () => {
             </small>
           </div>
         </article>
+
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVTR1} alt="Avatar One" />
+            <h5 className="client__name">Ernest Achiever</h5>
+            <small className="client__review">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam,
+              asperiores neque fuga necessitatibus corporis dolor. Dolor fuga
+              eos, blanditiis necessitatibus dolores facilis hic nesciunt labore
+              dolore, aliquam optio, harum alias.
+            </small>
+          </div>
+        </article>
+
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVTR1} alt="Avatar One" />
+            <h5 className="client__name">Ernest Achiever</h5>
+            <small className="client__review">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam,
+              asperiores neque fuga necessitatibus corporis dolor. Dolor fuga
+              eos, blanditiis necessitatibus dolores facilis hic nesciunt labore
+              dolore, aliquam optio, harum alias.
+            </small>
+          </div>
+        </article>
+
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVTR1} alt="Avatar One" />
+            <h5 className="client__name">Ernest Achiever</h5>
+            <small className="client__review">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam,
+              asperiores neque fuga necessitatibus corporis dolor. Dolor fuga
+              eos, blanditiis necessitatibus dolores facilis hic nesciunt labore
+              dolore, aliquam optio, harum alias.
+            </small>
+          </div>
+        </article>
       </div>
     </section>
   );
