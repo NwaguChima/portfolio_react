550af3ffe343f0cd993cc5b3ecd10e84645f6d10
NwaguChima <nwaguchima48@gmail.com>
2023-03-04 11:10:27 +0100

    completed testi styles

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -16,53 +16,53 @@ const Testimonials = () => {
         <article className="testimonial">
           <div className="client__avatar">
             <img src={AVTR1} alt="Avatar One" />
-            <h5 className="client__name">Ernest Achiever</h5>
-            <small className="client__review">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam,
-              asperiores neque fuga necessitatibus corporis dolor. Dolor fuga
-              eos, blanditiis necessitatibus dolores facilis hic nesciunt labore
-              dolore, aliquam optio, harum alias.
-            </small>
           </div>
+          <h5 className="client__name">Ernest Achiever</h5>
+          <small className="client__review">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam,
+            asperiores neque fuga necessitatibus corporis dolor. Dolor fuga eos,
+            blanditiis necessitatibus dolores facilis hic nesciunt labore
+            dolore, aliquam optio, harum alias.
+          </small>
         </article>
 
         <article className="testimonial">
           <div className="client__avatar">
             <img src={AVTR1} alt="Avatar One" />
-            <h5 className="client__name">Ernest Achiever</h5>
-            <small className="client__review">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam,
-              asperiores neque fuga necessitatibus corporis dolor. Dolor fuga
-              eos, blanditiis necessitatibus dolores facilis hic nesciunt labore
-              dolore, aliquam optio, harum alias.
-            </small>
           </div>
+          <h5 className="client__name">Ernest Achiever</h5>
+          <small className="client__review">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam,
+            asperiores neque fuga necessitatibus corporis dolor. Dolor fuga eos,
+            blanditiis necessitatibus dolores facilis hic nesciunt labore
+            dolore, aliquam optio, harum alias.
+          </small>
         </article>
 
         <article className="testimonial">
           <div className="client__avatar">
             <img src={AVTR1} alt="Avatar One" />
-            <h5 className="client__name">Ernest Achiever</h5>
-            <small className="client__review">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam,
-              asperiores neque fuga necessitatibus corporis dolor. Dolor fuga
-              eos, blanditiis necessitatibus dolores facilis hic nesciunt labore
-              dolore, aliquam optio, harum alias.
-            </small>
           </div>
+          <h5 className="client__name">Ernest Achiever</h5>
+          <small className="client__review">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam,
+            asperiores neque fuga necessitatibus corporis dolor. Dolor fuga eos,
+            blanditiis necessitatibus dolores facilis hic nesciunt labore
+            dolore, aliquam optio, harum alias.
+          </small>
         </article>
 
         <article className="testimonial">
           <div className="client__avatar">
             <img src={AVTR1} alt="Avatar One" />
-            <h5 className="client__name">Ernest Achiever</h5>
-            <small className="client__review">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam,
-              asperiores neque fuga necessitatibus corporis dolor. Dolor fuga
-              eos, blanditiis necessitatibus dolores facilis hic nesciunt labore
-              dolore, aliquam optio, harum alias.
-            </small>
           </div>
+          <h5 className="client__name">Ernest Achiever</h5>
+          <small className="client__review">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam,
+            asperiores neque fuga necessitatibus corporis dolor. Dolor fuga eos,
+            blanditiis necessitatibus dolores facilis hic nesciunt labore
+            dolore, aliquam optio, harum alias.
+          </small>
         </article>
       </div>
     </section>
