f4007f6724edf78879517f1cc011e0700ffb547b
NwaguChima <nwaguchima48@gmail.com>
2023-03-04 08:23:36 +0100

    added second article markup

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -38,6 +38,34 @@ const Services = () => {
           </ul>
         </article>
         {/* end of first article */}
+        <article className="service">
+          <div className="service__head">
+            <h3>UI/UX Design</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <GiCheckMark className="service__list-icon" />
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <GiCheckMark className="service__list-icon" />
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <GiCheckMark className="service__list-icon" />
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <GiCheckMark className="service__list-icon" />
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <GiCheckMark className="service__list-icon" />
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </li>
+          </ul>
+        </article>
       </div>
     </section>
   );
