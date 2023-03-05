660f311396a14f5e2bd726fb516d534dabb7decc
NwaguChima <nwaguchima48@gmail.com>
2023-03-05 15:28:58 +0100

    cleanup about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -8,9 +8,6 @@ import './about.css';
 const About = () => {
   return (
     <section id="about">
-      {/* <h5>Get To Know</h5>
-      <h2>About Me</h2> */}
-
       <div className="container about__container">
         <div className="about__me">
           <div className="about__me-image">
@@ -52,10 +49,6 @@ const About = () => {
             continuously improving skills to create engaging and high-quality
             user experiences.
           </p>
-
-          {/* <a href="#contact" className="btn btn-primary">
-            Let's Talk
-          </a> */}
         </div>
       </div>
     </section>
